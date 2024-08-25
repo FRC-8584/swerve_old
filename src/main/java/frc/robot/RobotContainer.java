2507788f0195f962353483948519c5727949210c
yellowcat9487 <yellowcatwhat@gmail.com>
2024-08-25 20:26:55 +0800

    wtf

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 
 /*** subsystems ***/
 
@@ -29,5 +31,7 @@ public class RobotContainer {
     );
   }
 
-  
+    public Command getAutonomousCommand() {
+    return new PrintCommand("None auto command running!");
+  }
 }
